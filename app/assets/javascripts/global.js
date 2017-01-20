b2556b3f5ea2abcf96c922b02596173ade5ce14a
Rachel Lyons <rlyons21@hotmail.com>
2017-01-20 16:50:51 -0600

    js for gallery works now... no turbolinks

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,17 +1,19 @@
 
 window.addEventListener("load", function(){
-	var modal = document.getElementsByClassName("modal");
-	var pictures = document.getElementsByClassName("thumbnail");
+	// $(document).on('ready page:load', function() {
+		var modal = document.getElementsByClassName("modal");
+		var pictures = document.getElementsByClassName("thumbnail");
 
 
-	for(i=0; i < pictures.length; i++){
-		pictures[i].addEventListener("click", function(e){
-			var content = document.getElementById("modalImage");
-			var clicked = this.getAttribute("id");
-			
-			content.setAttribute("src", "assets/" + clicked);
+		for(i=0; i < pictures.length; i++){
+			pictures[i].addEventListener("click", function(e){
+				var content = document.getElementById("modalImage");
+				var clicked = this.getAttribute("id");
+				
+				content.setAttribute("src", "assets/" + clicked);
 
-			
-		});
-	};
+				
+			});
+		};
+	// });
 });
